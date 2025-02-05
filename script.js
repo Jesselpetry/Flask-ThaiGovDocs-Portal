1b0af35703cac2b304b3dea3841042b3f627df67
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2025-02-06 06:59:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     mainButton.addEventListener('click', (e) => {
         e.preventDefault();
         // Add your link here
-        window.location.href = 'https://youtu.be/nmn9mXBCsU8?si=ydVsEUpI-54oGabI';
+        window.location.href = 'https://3ab7-2403-6200-8832-2fc2-60f9-a83b-4340-4944.ngrok-free.app/';
     });
 });
